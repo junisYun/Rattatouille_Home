2512cc769c882becdeaf39c6cd8e4774fa3bab5a
컴퓨터 하는 라따뚜이 <81808703+junisYun@users.noreply.github.com>
2021-07-27 00:55:03 +0900

    JS: NavbarMenu__scrollEvent

--- main.js
+++ main.js
@@ -3,11 +3,57 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbar: ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+})
+
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView();
+});
+
+// handle scrolling when tapping Contact button
+const query = document.querySelector('.home__contact');
+query.addEventListener('click', () => {
+    const scrollTo = document.querySelector('#contact');
+    scrollTo.scrollIntoView(
+        {
+            behavior: "smooth"
+        }
+    );
+})
+
+// wheel test
+let cnt = 0;
+document.querySelector('body').addEventListener('wheel', (event) => {
+    // if (event.deltaY < 0 && window.scrollY > 100) {
+    //     navbar.classList.add('navbar--wheelDown');
+    // } else {
+    //     navbar.classList.remove('navbar--wheelDown');
+    // }
+    if (window.scrollY < 100) {
+        navbar.classList.remove('navbar--wheelDown');
+        return;
+    }
+    if (event.deltaY < 0) {
+        cnt++;
+    } else {
+        cnt = 0;
+    }
+    if (cnt > 5) {
+        navbar.classList.add('navbar--wheelDown');
+    } else {
+        navbar.classList.remove('navbar--wheelDown');
+    }
+    console.log(cnt);
 })
